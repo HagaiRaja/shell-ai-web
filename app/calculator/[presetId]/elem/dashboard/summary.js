bb0f82a443887b5ac3e332f502f08da18aae35bd
Hagai Raja <hagairaja@telepix.net>
2024-08-05 18:30:55 +0900

    solve apex window undefined error

--- app/calculator/[presetId]/elem/dashboard/summary.js
+++ app/calculator/[presetId]/elem/dashboard/summary.js
@@ -1,8 +1,9 @@
 
+import dynamic from 'next/dynamic';
 import { Button, Container, Row, Col, Form, Accordion } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useDispatch, useSelector } from 'react-redux';
-import Chart from 'react-apexcharts'
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 export function Summary() {
   const allData = useSelector((state) => state.data.value);
