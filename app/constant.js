e03c1c78c4c6dcff91dfc4c1bb1f146d723531be
Hagai Raja <hagairaja@telepix.net>
2024-08-17 22:57:44 +0900

    add constant default value

--- app/constant.js
+++ app/constant.js
@@ -1,4 +1,4 @@
 import { join } from 'path'
 
 export const PRESET_DIR = join(process.cwd(), "public", "preset")
-export const URL = process.env.NEXT_PUBLIC_URL;
+export const URL = (process.env.NEXT_PUBLIC_URL) ? process.env.NEXT_PUBLIC_URL : "http://localhost:3000";
