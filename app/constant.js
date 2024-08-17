9b6c448cac7875dd8ef289856209a3f3637b312d
Hagai Raja <hagairaja@telepix.net>
2024-08-17 22:44:08 +0900

    fix env variable

--- app/constant.js
+++ app/constant.js
@@ -1,4 +1,4 @@
-import { resolve } from 'path'
+import { join } from 'path'
 
-export const PRESET_DIR = resolve(process.env.NEXT_PUBLIC_ROOT_PATH ?? "", "public/preset");
+export const PRESET_DIR = join(process.cwd(), "public", "preset")
 export const URL = process.env.NEXT_PUBLIC_URL;
